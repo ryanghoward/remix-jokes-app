823500739a4620ed7073471b0c11318669739660
Ryan Howard <ryanghoward@protonmail.com>
2022-09-22 16:08:34 -0700

    save

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,26 @@
 import type { LinksFunction } from "@remix-run/node";
 import stylesUrl from "~/styles/index.css";
+import { Link } from "@remix-run/react";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
 export default function IndexRoute() {
-  return <div>Hello Index Route</div>;
+  return (
+    <div className='container'>
+      <div className='content'>
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to='jokes'>Click me for Jokes!</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
